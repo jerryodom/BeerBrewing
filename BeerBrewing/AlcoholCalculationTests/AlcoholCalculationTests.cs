f529a50aa5a987bf9bb41f5439c23a6219440024
jerryodom <jerry@jerryodom.com>
2016-02-28 13:29:27 -0600

    move alcohol calculations

--- BeerBrewing/AlcoholCalculationTests/AlcoholCalculationTests.cs
+++ BeerBrewing/AlcoholCalculationTests/AlcoholCalculationTests.cs
@@ -12,15 +12,40 @@ namespace AlcoholCalculationTests
         public void ABVCalculation_Passes()
         {
             ICalculateAlcoholFactory calculatorFactory = new CalculateAlcoholFactory();
-            ICalculateAlcohol calculator = calculatorFactory.GetCalculator("Real");
+            ICalculateAlcohol calculator = calculatorFactory.GetCalculator("ABV");
             calculator.StartingGravity = 1.05;
             calculator.EndingGravity = 1.01;
-            var realAlcohol = (calculator as Calculator).Calculate();
-            Assert.AreEqual(65.72584, realAlcohol);
+            var abvAlcohol = (calculator as Calculator).Calculate();
+            Assert.AreEqual(5.26381752099261, abvAlcohol);
         }
         [TestMethod]
         public void ABWCalculation_Passes()
         {
+            {
+                ICalculateAlcoholFactory calculatorFactory = new CalculateAlcoholFactory();
+                ICalculateAlcohol calculator = calculatorFactory.GetCalculator("ABW");
+                calculator.StartingGravity = 1.05;
+                calculator.EndingGravity = 1.01;
+                var abwAlcohol = (calculator as Calculator).Calculate();
+                Assert.AreEqual(4.1761022684546969, abwAlcohol);
+            }
+        }
+        [TestMethod]
+        public void AlcoholCalculationTestMethod_Fails_BadArgument()
+        {
+            try
+            {
+                ICalculateAlcoholFactory calculatorFactory = new CalculateAlcoholFactory();
+                ICalculateAlcohol calculator = calculatorFactory.GetCalculator("garbage");
+                calculator.StartingGravity = 1.05;
+                calculator.EndingGravity = 1.01;
+                var alcoholCalculator = (calculator as Calculator).Calculate();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(ArgumentOutOfRangeException));
+            }
+
         }
     }
 }
