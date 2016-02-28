e2978fa42bb4abb9b04f68cf43fe5c3c0718cf79
jerryodom <jerry@jerryodom.com>
2016-02-28 11:39:30 -0600

    alcohol by volume

--- BeerBrewing/AlcoholCalculation/AlcoholCalculation.cs
+++ BeerBrewing/AlcoholCalculation/AlcoholCalculation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,54 @@ using System.Threading.Tasks;
 
 namespace AlcoholCalculation
 {
-    public class AlcoholCalculation
+    public interface ICalculateAlcoholFactory
     {
+        ICalculateAlcohol GetCalculator(string CalculationType);
+
+    }
+
+    public class CalculateAlcoholFactory : ICalculateAlcoholFactory
+    {
+        public ICalculateAlcohol GetCalculator(string CalculationType)
+        {
+            if (CalculationType == "ABV")
+            {
+                return null;
+            }
+            else if (CalculationType == "ABW")
+            {
+                return null;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Only ABV and ABW are valid Alcohol values.");
+            }
+        }
+    }
+
+    public interface ICalculateAlcohol
+    {
+        double StartingGravity { get; set; }
+        double EndingGravity { get; set; }
+
+    }
+    public class AlcoholByVolumeCalculation : Calculator, ICalculateAlcohol
+    {
+        public double EndingGravity
+        {
+            get; set;
+        }
+
+        public double StartingGravity
+        {
+            get; set;
+        }
+
+        public override double Calculate()
+        {
+            if (this.EndingGravity == 1)
+                throw new ArgumentOutOfRangeException("EndingGravity", 1, "1 is not a valid value for EndingGravity");
+            return ((((double)1.05 * (this.StartingGravity - this.EndingGravity)) / this.EndingGravity) / (double)0.79) * 100;
+        }
     }
 }
---
+++ BeerBrewing/AlcoholCalculationTests/AlcoholCalculationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlcoholCalculationTests
+{
+    [TestClass]
+    public class AlcoholCalculationTests
+    {
+        [TestMethod]
+        public void ABVCalculation_Passes()
+        {
+        }
+        [TestMethod]
+        public void ABWCalculation_Passes()
+        {
+        }
+    }
+}
