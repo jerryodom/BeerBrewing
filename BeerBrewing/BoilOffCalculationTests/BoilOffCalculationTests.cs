eda05bf1dc6a63b369c3f710d61cd102d84cf51e
jerryodom <jerry@jerryodom.com>
2016-02-28 19:27:40 -0600

    baseline boil off code.

---
+++ BeerBrewing/BoilOffCalculationTests/BoilOffCalculationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoilOffCalculationTests
+{
+    [TestClass]
+    public class BoilOffCalculationTests
+    {
+        [TestMethod]
+        public void CalculateBoilOff_Passes()
+        {
+        }
+    }
+}
---
+++ BeerBrewing/BoilOffCalculator/BoilOffCalculation.cs
@@ -0,0 +1,56 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoilOffCalculator
+{
+    public class BoilOffCalculation
+    {
+        public interface ICalculateBoilOffFactory
+        {
+            ICalculateBoilOff GetCalculator(string CalculationType);
+
+        }
+
+        public class CalculateBoilOffFactory : ICalculateBoilOffFactory
+        {
+            public ICalculateBoilOff GetCalculator(string CalculationType)
+            {
+                return new BoilOffByVolumeCalculation();
+            }
+        }
+
+        public interface ICalculateBoilOff
+        {
+            double BoilTimeInMinutes { get; set; }
+            double StartingVolumeInGallons { get; set; }
+            double EvaporationRateInPercent { get; set; }
+
+        }
+        public class BoilOffByVolumeCalculation : Calculator, ICalculateBoilOff
+        {
+            public double BoilTimeInMinutes
+            {
+                get; set;
+            }
+
+            public double StartingVolumeInGallons
+            {
+                get; set;
+            }
+
+            public double EvaporationRateInPercent
+            {
+                get; set;
+            }
+
+            public override double Calculate()
+            {
+                 return (this.StartingVolumeInGallons * (this.EvaporationRateInPercent / 100)) * (this.BoilTimeInMinutes / 60);
+            }
+        }
+    }
+}
