1e6d535403e60d3e94c4ed2adbec39c1df478d0f
jerryodom <jerry@jerryodom.com>
2016-03-30 13:34:53 -0500

    Calculate Final Volume

---
+++ BeerBrewing/FinalVolumeCalculation/FinalVolumeCalculation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace FinalVolumeCalculation
+{
+    public interface ICalculateBoilOffFactory
+    {
+        ICalculateFinalVolume GetCalculator(string CalculationType);
+    }
+    public class CalculateBoilOffFactory : ICalculateBoilOffFactory
+    {
+        public ICalculateFinalVolume GetCalculator(string CalculationType)
+        {
+            return new FinalVolumeCalculation();
+        }
+    }
+
+    public class FinalVolumeCalculation : Calculator, ICalculateFinalVolume
+    {
+        public double BoilOffInGallons
+        {
+            get; set;
+        }
+
+        public double LossFromCoolingShrinkageInGallons
+        {
+            get; set;
+        }
+
+        public List<double> OtherValuesToSubtractFromStartingVolume
+        {
+            get; set;
+        }
+
+        public double StartingVolumeInGallons
+        {
+            get; set;
+        }
+
+        public override double Calculate()
+        {
+            var valueForReturn = StartingVolumeInGallons - BoilOffInGallons - LossFromCoolingShrinkageInGallons;
+            if(OtherValuesToSubtractFromStartingVolume != null && OtherValuesToSubtractFromStartingVolume.Count != 0)
+            {
+                foreach(var valueToSubtract in OtherValuesToSubtractFromStartingVolume)
+                {
+                    valueForReturn -= valueToSubtract;
+                }
+            }
+            if(valueForReturn < 0)
+            {
+                throw new ArgumentOutOfRangeException("StartingVolumeInGallons", "Your starting volume is less than your combined boil off, loss from shrinkage and other values.  Can't have a negative final volume");
+            }
+            return valueForReturn;
+        }
+    }
+
+    public interface ICalculateFinalVolume
+    {
+        double StartingVolumeInGallons { get; set; }
+
+        double BoilOffInGallons { get; set; }
+
+        double LossFromCoolingShrinkageInGallons { get; set; }
+
+        List<double> OtherValuesToSubtractFromStartingVolume { get; set; }
+    }
+}
---
+++ BeerBrewing/FinalVolumeCalculationTests/FinalVolumeCalculationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalVolumeCalculation;
+using Core;
+
+namespace FinalVolumeCalculationTests
+{
+    [TestClass]
+    public class FinalVolumeCalculationTests
+    {
+        [TestMethod]
+        public void FinalVolume_CalculationTest()
+        {
+            ICalculateBoilOffFactory volumeCalculationFactory = new CalculateBoilOffFactory();
+            var calculator = volumeCalculationFactory.GetCalculator(null);
+            //possibly should abstract this out further in to a Filter in case people want different parameters in boil off calculation.
+            calculator.BoilOffInGallons = .2;
+            calculator.LossFromCoolingShrinkageInGallons = .192;
+            calculator.StartingVolumeInGallons = 5;
+            var finalVolume = (calculator as Calculator).Calculate();
+            Assert.AreEqual(finalVolume, 4.608);
+
+        }
+    }
+}
