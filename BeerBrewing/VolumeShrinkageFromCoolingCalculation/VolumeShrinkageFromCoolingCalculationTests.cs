c1735271b1827d4f4bab4927347903a8d5511a68
jerryodom <jerry@jerryodom.com>
2016-03-30 11:43:13 -0500

    Boil off and Shrinkage from Cooling.

---
+++ BeerBrewing/VolumeShrinkageFromCoolingCalculation/VolumeShrinkageFromCoolingCalculationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Core;
+using BoilOffCalculaton;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VolumeShrinkageFromCoolingCalculation
+{
+    [TestClass]
+    public class VolumeShrinkageFromCoolingCalculationTests
+    {
+        [TestMethod]
+        public void CalculateVolumeShrinkageFromCooling_Passes()
+        {
+
+            ICalculateBoilOffFactory boilCalculatorFactory = new CalculateBoilOffFactory();
+            ICalculateBoilOff boilCalculator = boilCalculatorFactory.GetCalculator(null);
+            boilCalculator.BoilTimeInMinutes = 60;
+            boilCalculator.StartingVolumeInGallons = 5;
+            boilCalculator.EvaporationRateInPercent = 4;
+            var boiledOffVolumeInGalls = (boilCalculator as Calculator).Calculate();
+            Assert.AreEqual(0.2, boiledOffVolumeInGalls);
+
+            ICalculateVolumeShrinkageFromCoolingFactory calculatorFactory = new CalculateVolumeShrinkageFromCoolingFactory();
+            ICalculateVolumeShrinkageFromCooling calculator = calculatorFactory.GetCalculator(null);
+            calculator.VolumeAfterBoilOff =  5 - boiledOffVolumeInGalls;
+            calculator.StartingVolumeInGallons = 5;
+            calculator.CoolingLossInPercent = 4;
+            var shrinkageFromCoolingInGals = (calculator as Calculator).Calculate();
+            Assert.AreEqual(0.192, shrinkageFromCoolingInGals);
+        }
+    }
+}
---
+++ BeerBrewing/VolumeShrinkageFromCoolingCalculationClass/VolumeShrinkageFromCoolingCalculation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace VolumeShrinkageFromCoolingCalculation
+{
+    public interface ICalculateVolumeShrinkageFromCoolingFactory
+    {
+        ICalculateVolumeShrinkageFromCooling GetCalculator(string CalculationType);
+
+    }
+
+    public class CalculateVolumeShrinkageFromCoolingFactory : ICalculateVolumeShrinkageFromCoolingFactory
+    {
+        public ICalculateVolumeShrinkageFromCooling GetCalculator(string CalculationType)
+        {
+            return new VolumeShrinkageFromCoolingCalculation();
+        }
+    }
+    public class VolumeShrinkageFromCoolingCalculation : Calculator, ICalculateVolumeShrinkageFromCooling
+    {
+        public double CoolingLossInPercent
+        {
+            get; set;
+        }
+
+        public double StartingVolumeInGallons
+        {
+            get; set;
+        }
+
+        public double VolumeAfterBoilOff
+        {
+            get; set;
+        }
+
+        public override double Calculate()
+        {
+            return VolumeAfterBoilOff * CoolingLossInPercent / 100;
+        }
+    }
+
+
+
+
+    public interface ICalculateVolumeShrinkageFromCooling
+    {
+        double VolumeAfterBoilOff { get; set; }
+        double StartingVolumeInGallons { get; set; }
+        double CoolingLossInPercent { get; set; }
+
+    }
+}
