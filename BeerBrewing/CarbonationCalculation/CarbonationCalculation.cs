f52714b5522e4e5bf916628d48cc3fa05cd40aff
jerryodom <jerry@jerryodom.com>
2016-03-31 23:36:32 -0500

    Base carbonation code.  First strategy.

---
+++ BeerBrewing/CarbonationCalculation/CarbonationCalculation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace CarbonationCalculation
+{
+
+
+    public class CalculateCarbonationFactory : ICalculateCarbonationFactory
+    {
+        public ICalculateCarbonation GetCalculator(string CalculationType)
+        {
+            return new CarbonationCalculation();
+        }
+    }
+    public interface ICalculateCarbonationFactory
+    {
+        ICalculateCarbonation GetCalculator(string CalculationType);
+    }
+
+
+    public interface ICarbonationStrategy
+    {
+        double CalculateCarbonation(CarbonationCalculation carbonationDetails);
+    }
+
+    public class CarbonateUsingDmeStrategy : ICarbonationStrategy
+    {
+        public double CalculateCarbonation(CarbonationCalculation carbonationDetails)
+        {
+            return ((double)21.27 * carbonationDetails.BeerVolumeInGallons) * (carbonationDetails.DesiredCarbonationInVolumes - (double)3.0378 + (double).050062 * carbonationDetails.BeerTemperatureInFahrenheit - (double).00026555 * carbonationDetails.BeerTemperatureInFahrenheit * carbonationDetails.BeerTemperatureInFahrenheit);
+        }
+    }
+
+
+    public class CarbonateUsingForcedPressureStrategy : ICarbonationStrategy
+    {
+        public double CalculateCarbonation(CarbonationCalculation carbonationDetails)
+        {
+            return (double)-16.6999 - ((double)0.0101059 * carbonationDetails.BeerTemperatureInFahrenheit) + ((double)0.00116512 * carbonationDetails.BeerTemperatureInFahrenheit * carbonationDetails.BeerTemperatureInFahrenheit) + ((double)0.173354 * carbonationDetails.BeerTemperatureInFahrenheit * carbonationDetails.DesiredCarbonationInVolumes) + ((double)4.24267 * carbonationDetails.DesiredCarbonationInVolumes) - ((double)0.0684226 * carbonationDetails.DesiredCarbonationInVolumes * carbonationDetails.DesiredCarbonationInVolumes);
+        }
+    }
+
+
+    public interface ICalculateCarbonation
+    {
+        double BeerVolumeInGallons { get; set; }
+        double BeerTemperatureInFahrenheit { get; set; }
+
+        double DesiredCarbonationInVolumes { get; set; }
+        
+        void SetCarbonationType(ICarbonationStrategy carbonationStrategy);
+    }
+
+    public class CarbonationCalculation : Calculator, ICalculateCarbonation
+    {
+        public double BeerTemperatureInFahrenheit
+        {
+            get; set;
+        }
+
+        public double BeerVolumeInGallons
+        {
+            get; set;
+        }
+
+        public double DesiredCarbonationInVolumes
+        {
+            get; set;
+        }
+
+        public void SetCarbonationType(ICarbonationStrategy strategy)
+        {
+            this.CarbonationType = strategy;
+        }
+
+        ICarbonationStrategy CarbonationType
+        {
+            get; set;
+        }
+        
+
+        public override double Calculate()
+        {
+            return this.CarbonationType.CalculateCarbonation(this);
+        }
+    }
+}
---
+++ BeerBrewing/CarbonationCalculationTests/CarbonationCalculationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using CarbonationCalculation;
+
+namespace CarbonationCalculationTests
+{
+    [TestClass]
+    public class CarbonationCalculationTests
+    {
+        [TestMethod]
+        public void CarbonationCalculation_Passes()
+        {
+            ICalculateCarbonationFactory carbonationFactory = new CalculateCarbonationFactory();
+            var carbonationCalculator = carbonationFactory.GetCalculator(null);
+            carbonationCalculator.BeerVolumeInGallons = 5;
+            carbonationCalculator.BeerTemperatureInFahrenheit = 32;
+            carbonationCalculator.DesiredCarbonationInVolumes = 3;
+            carbonationCalculator.SetCarbonationType(new CarbonateUsingForcedPressureStrategy());
+            double carbonationPressureNeeded = (carbonationCalculator as Calculator).Calculate();
+            Assert.AreEqual(12.92, Math.Round(carbonationPressureNeeded, 2));
+
+        }
+    }
+}
