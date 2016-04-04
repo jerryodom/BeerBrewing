6a93e820db0f2a03c014f3bd5f0d7cfbbc888a9c
jerryodom <jerry@jerryodom.com>
2016-04-04 12:40:48 -0500

    adding references

--- BeerBrewing/WaterNeededExtractCalculation/WaterNeededExtractCalculation.cs
+++ BeerBrewing/WaterNeededExtractCalculation/WaterNeededExtractCalculation.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core;
 
 namespace WaterNeededExtractCalculation
 {
-    public class WaterNeededExtractCalculation
+    public interface ICalculateWaterNeededExtractFactory
     {
+
+    }
+
+    public class CalculateWaterNeededExtractFactory : ICalculateWaterNeededExtractFactory
+    {
+        public ICalculateWaterNeededExtract GetCalculator(string CalculationType)
+        {
+            return null;
+        }
+    }
+
+    public interface ICalculateWaterNeededExtract
+    {
+
+    }
+
+    public class WaterNeededExtractCalculation : Calculator, ICalculateWaterNeededExtract
+    {
+        public override double Calculate()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
