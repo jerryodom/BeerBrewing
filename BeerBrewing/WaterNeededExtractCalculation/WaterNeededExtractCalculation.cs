8a59984fd49c7f21080bf4dd858e9bbe6e6ad679
jerryodom <jerry@jerryodom.com>
2016-04-12 15:10:17 -0500

    WaterNeededExtract Version1

--- BeerBrewing/WaterNeededExtractCalculation/WaterNeededExtractCalculation.cs
+++ BeerBrewing/WaterNeededExtractCalculation/WaterNeededExtractCalculation.cs
@@ -9,6 +9,7 @@ namespace WaterNeededExtractCalculation
 {
     public interface ICalculateWaterNeededExtractFactory
     {
+        ICalculateWaterNeededExtract GetCalculator(string CalculationType);
 
     }
 
@@ -16,20 +17,114 @@ namespace WaterNeededExtractCalculation
     {
         public ICalculateWaterNeededExtract GetCalculator(string CalculationType)
         {
-            return null;
+            return new WaterNeededExtractCalculation();
         }
     }
 
     public interface ICalculateWaterNeededExtract
     {
+        double MashWaterVolumeInQuarts { get; set; }
+        
+        int BoilTimeInMinutes { get; set; }
 
+        double EvaporationRatePercent { get; set; }
+
+        double TopUpWaterInGallons { get; set; }
+
+        double DesiredFinalVolumeInGallons { get; set; }
+
+        double LossToTrubInGallons { get; set; }
+
+        double GetTotalWaterNeededInGallons();
+
+        double GetBoiledOffVolumeInGallons();
+        
+        double GetBoilVolumeInGallons();
+
+        double GetCoolingLossInGallons();
     }
 
     public class WaterNeededExtractCalculation : Calculator, ICalculateWaterNeededExtract
     {
+
+        public double GetBoiledOffVolumeInGallons()
+        {
+            return 0;
+        }
+
+        public int BoilTimeInMinutes
+        {
+            get; set;
+        }
+
+        public double GetBoilVolumeInGallons()
+        {
+                double step1 = (this.DesiredFinalVolumeInGallons - this.TopUpWaterInGallons + this.LossToTrubInGallons) * (this.EvaporationRatePercent / 100) * (this.BoilTimeInMinutes / 60);
+                double step2 = (step1 +this.GetCoolingLossInGallons()) + (((step1 + this.GetCoolingLossInGallons()) * (this.EvaporationRatePercent / 100) * this.BoilTimeInMinutes / 60));
+                double boilVolume = this.DesiredFinalVolumeInGallons + step2 - this.TopUpWaterInGallons + this.LossToTrubInGallons;
+                return boilVolume;
+            
+        }
+
+        public double CoolingLossVolumeInGallons
+        {
+            get; set;
+        }
+
+        public double DesiredFinalVolumeInGallons
+        {
+            get; set;
+        }
+
+        public double EvaporationRatePercent
+        {
+            get; set;
+        }
+
+
+        public double MashWaterVolumeInQuarts
+        {
+            get; set;
+        }
+        
+        public double TopUpWaterInGallons
+        {
+            get; set;
+        }
+
+        public double GetCoolingLossInGallons()
+        {
+                return this.DesiredFinalVolumeInGallons * .041666;   //this .041666 number is a boil off constant
+            
+        }
+
+        public double LossToTrubInGallons
+        {
+            get; set;
+        }
+
+        public double GetTotalWaterNeededInGallons()
+        {
+                double ltt = this.LossToTrubInGallons;
+                double tup = this.TopUpWaterInGallons;
+                double dfv = this.DesiredFinalVolumeInGallons;
+            double coolingLoss = this.GetCoolingLossInGallons();
+                double X1 = (dfv - tup + ltt) * (this.EvaporationRatePercent / 100) * ((double)this.BoilTimeInMinutes /60);
+                double X2 = (X1 + coolingLoss) + (((X1 + coolingLoss) * (this.EvaporationRatePercent / 100) * (double)this.BoilTimeInMinutes /60));
+                double totalWaterNeeded = dfv + X2 + ltt;
+
+            return Math.Round(totalWaterNeeded, 2);
+            
+        }
+
+
         public override double Calculate()
         {
-            throw new NotImplementedException();
+            //returns total water needed
+            return this.GetTotalWaterNeededInGallons();
+
         }
+
+
     }
 }
--- BeerBrewing/WaterNeededExtractCalculationTests/WaterNeededExtractCalculationTests.cs
+++ BeerBrewing/WaterNeededExtractCalculationTests/WaterNeededExtractCalculationTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using WaterNeededExtractCalculation;
 
 namespace WaterNeededExtractCalculationTests
 {
@@ -7,8 +9,20 @@ namespace WaterNeededExtractCalculationTests
     public class WaterNeededExtractCalculationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void WaterNeededExtractCalculation_Passes()
         {
+            ICalculateWaterNeededExtractFactory waterNeededFactory = new CalculateWaterNeededExtractFactory();
+            ICalculateWaterNeededExtract waterNeededExtractCalculator = waterNeededFactory.GetCalculator(null);
+            waterNeededExtractCalculator.BoilTimeInMinutes = 60;
+            waterNeededExtractCalculator.DesiredFinalVolumeInGallons = 6;
+            waterNeededExtractCalculator.EvaporationRatePercent = 12.79;
+            waterNeededExtractCalculator.LossToTrubInGallons = 0.5;
+            waterNeededExtractCalculator.MashWaterVolumeInQuarts = 16;
+            waterNeededExtractCalculator.TopUpWaterInGallons = 0;
+            waterNeededExtractCalculator.GetCoolingLossInGallons();
+            var totalWaterNeeded = (waterNeededExtractCalculator as Calculator).Calculate();
+            Assert.AreEqual(7.72, totalWaterNeeded);
+
         }
     }
 }
