5dc61a0e83b517fa291f612cbabd0687334ef38b
jerryodom <jerry@jerryodom.com>
2016-05-20 11:09:56 -0500

    node stuff

---
+++ BeerBrewing/BeerBrewing/routes/beermath.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('beermath', { title: 'BeerMath API' });
+    
+});
+
+
+router.route('/abv')
+    .get(function (req, res)
+    {
+        res.send("getting the abv dawg");
+    });
+
+module.exports = router;
