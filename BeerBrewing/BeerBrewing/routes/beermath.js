3ae99faf94aafca379576c77f46195ad8f559ad7
jerryodom <jerry@jerryodom.com>
2016-05-24 10:53:22 -0500

    baseline node/express service calling the web api service on your localhost for now.

--- BeerBrewing/BeerBrewing/routes/beermath.js
+++ BeerBrewing/BeerBrewing/routes/beermath.js
@@ -1,4 +1,6 @@
-﻿var express = require('express');
+﻿var http = require('http');
+var serviceUrl = "localhost";
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,10 +10,25 @@ router.get('/', function (req, res) {
 });
 
 
-router.route('/abv')
-    .get(function (req, res)
-    {
-        res.send("getting the abv dawg");
-    });
+router.route('/abv/:starting_gravity/:final_gravity/:temp_fahrenheit').get(function (req, res){
+    var calloptions = {
+            host : serviceUrl, 
+            port : 80,
+            path : '/BeerBrewingApi/api/brewing/alcoholbyvolume/' + req.params.starting_gravity + '/' + req.params.final_gravity + '/' + req.params.temp_fahrenheit, 
+            method : 'GET'
+    };
+    console.info(calloptions);
+    var callback = function (res2) {
+        var content = "";
+        res2.on('data', function (chunk) {
+            content += chunk;
+        });
+        res2.on('end', function () {
+            theresults = content;
+            res.send(theresults);
+        });
+    };
+    http.request(calloptions, callback).end();
+});
 
 module.exports = router;
