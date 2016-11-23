a8d8c4c5655ad9fee2ae1355cde3318c473ff78f
jerryodom <jerry@jerryodom.com>
2016-11-23 14:02:33 -0600

    Need to test EF data model.

---
+++ BeerBrewing/BrewingRecipes.Data.Tests/BeerBrewingRecipes.Data.Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrewingRecipes.Data.Tests
+{
+    [TestClass]
+    public class BeerBrewingDataTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BeerBrewing/BrewingRecipes.Data.Tests/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿Going to attempt using "Effort" to test EF as historically I've had a hard time maintaining Mocks on it. 
+
+https://blog.goyello.com/2016/07/14/save-time-mocking-use-your-real-entity-framework-dbcontext-in-unit-tests/
